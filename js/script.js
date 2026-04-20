818759ced0eee454f629954d3e2f794fcd2df616
Nathalie Almeida <45579354+natdrabik@users.noreply.github.com>
2026-04-20 16:53:30 -0300

    animation

--- js/script.js
+++ js/script.js
@@ -31,12 +31,12 @@ function closeMenu() {
 
 gsap.timeline()
 .from('.livros-col--up', {
-    yPercent: 450,
+    yPercent: 350,
     scrub: 1,
     duration: 5
 })
 .from('.livros-col--down', {
-    yPercent: -450,
+    yPercent: -350,
     scrub: 1,
     duration: 5
 }, "<")
