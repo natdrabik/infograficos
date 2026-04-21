c3fdb9f922a80e295fa38a8dc9d2c1172ac7a73b
Nathalie Almeida <45579354+natdrabik@users.noreply.github.com>
2026-04-20 23:00:39 -0300

    animation

--- js/script.js
+++ js/script.js
@@ -43,21 +43,21 @@ gsap.timeline()
 .from('.intro p', {
     opacity: 0,
     // y: 50,
-    duration: 1,
+    duration: 0.5,
     // ease:"back"
 }, "<1")
 .from('.intro h1', {
     opacity: 0,
     // y: 50,
-    duration: 1, 
+    duration: 0.5, 
     // ease:"back"
-}, "<0.5")
+}, "<0.2")
 .from('.intro h2', {
     opacity: 0,
     // y: 50,
-    duration: 1, 
+    duration: 0.5, 
     // ease:"back"
-}, "<0.5")
+}, "<0.2")
 
 
 
